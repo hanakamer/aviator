3188358fbd17c5c930b4f663eff4eef8f4eace4a
hanakamer <hanakamer@gmail.com>
2016-05-01 18:48:09 +0300

    create sea

--- app/index.js
+++ app/index.js
@@ -16,11 +16,11 @@ function init() {
 
   createLigths();
 
-  createPlane();
+  // createPlane();
   createSea();
-  createSky();
+  // createSky();
 
-  loop();
+  // loop();
 
 };
 
@@ -74,4 +74,62 @@ function createScene() {
 
   window.addEventListener('resize', handleWindowResize, false);
 
+}
+
+function handleWindowResize() {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.aspect = WIDTH / HEIGHT;
+  camera.updateProjectMatrix();
+}
+
+let hemisphereLight;
+let shadowLight;
+
+function createLigths() {
+  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9);
+
+  shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+
+  shadowLight.position.set(150, 350, 350);
+
+  shadowLight.castShadow = true;
+
+  shadowLight.shadow.camera.left = -400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = -400;
+  shadowLight.shadow.camera.near = 1;
+  shadowLight.shadow.camera.far = 1000;
+
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+
+  scene.add(hemisphereLight);
+  scene.add(shadowLight);
+}
+
+const Sea = function() {
+  let geom =  new THREE.CylinderGeometry(600, 600, 800, 40,10);
+  geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+
+  let mat = new THREE.MeshPhongMaterial({
+    color: Colors.blue,
+    transparent: true,
+    opacity: .6,
+    shading: THREE.FlatShading
+  });
+
+  this.mesh = new THREE.Mesh(geom, mat);
+
+  this.mesh.receiveShadow = true;
+}
+
+let sea;
+
+function createSea() {
+  sea = new Sea();
+  sea.mesh.position.y = -600;
+  scene.add(sea.mesh);
 }
